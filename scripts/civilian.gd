6f0ef5d89a0cc4e84d0dab56cc89075b821a4300
Smugllar <smigglequiggle@gmail.com>
2023-08-12 15:43:02 -0500

    civilian stuff
    chahnged size of hitbox and set weight and stuff

---
+++ scripts/civilian.gd
@@ -0,0 +1,34 @@
+extends RigidBody2D
+
+const SPEED = 50.0
+
+#possibly confusingly, the higher then number, the slower it turns (maybe i should come up with a better variable name)
+var turn_speed = 2.0
+
+var move_dir = Vector2()
+
+
+func _physics_process(delta):
+	#delta but times 60 because _physics_process happesn 60 times a second
+	var relevantDelta = delta * 60.0
+	
+	#for if there is no movemetn!!!
+	move_dir = Vector2.ZERO
+	
+	#gets the direction that the character will be moving soon but in a coordinate sorta way
+	var direction = Vector2( Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down") )
+	
+	#i transfer the value of direction to a different variable because it soon gets changed
+	move_dir = direction
+	if move_dir != Vector2.ZERO:
+		move_dir = move_dir.normalized()
+	
+	linear_velocity += move_dir * SPEED * relevantDelta
+	
+	#turn towards the direction of movement
+	var angle_diff = 0 
+	var rotation_vector = Vector2(cos(rotation), sin(rotation))
+	if linear_velocity:
+		angle_diff = rotation_vector.angle_to(linear_velocity)
+	
+	angular_velocity += angle_diff / turn_speed
