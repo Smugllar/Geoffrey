66fcc34c6f327e613c443c73648ba4b3e489256e
Smugllar <smigglequiggle@gmail.com>
2023-08-12 12:54:19 -0500

    awesome movement
    things just got awesome

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,26 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 50.0
 const JUMP_VELOCITY = -400.0
 
-var move
+var move_dir = Vector2()
 
 
 func _physics_process(delta):
+	#delta but times 60 because _physics_process happesn 60 times a second
+	var relevantDelta = delta * 60
 	
-	#left righgt movement2!!!
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x *= 0.9
-
-	#up down omventment!!!!!!
-	direction = Input.get_axis("move_up", "move_down")
-	if direction:
-		velocity.y = direction * SPEED
-	else:
-		velocity.y *= 0.9
-
+	#for if there is no movemetn!!!
+	move_dir = Vector2.ZERO
+	
+	#gets the direction that the character will be moving soon but in a coordinate sorta way
+	var direction = Vector2( Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down") )
+	
+	#i transfer the value of direction to a different variable because it soon gets changed
+	move_dir = direction
+	if move_dir != Vector2.ZERO:
+		move_dir = move_dir.normalized()
+	
+	velocity += move_dir * SPEED * relevantDelta
+	velocity *= 0.9
+	
 	move_and_slide()
