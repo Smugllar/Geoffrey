4342dfe2c300bf23a1a8fed22de1b377eae1e817
Smugllar <smigglequiggle@gmail.com>
2023-08-14 20:53:20 -0500

    basic enrage stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,15 +2,16 @@ extends RigidBody2D
 
 const player = true
 
-const SPEED = 50.0
+var speed = 50.0
 
 #possibly confusingly, the higher the number, the slower it turns (maybe i should come up with a better variable name)
 var turn_speed = 2.0
 
 var move_dir = Vector2()
 var rage_dir = Vector2()
+var rage_priority = 0.0
 
-var enrage_level = 0
+var enrage_level = 1.0
 
 func _physics_process(delta):
 	var civilians = get_tree().get_nodes_in_group("civilians")
@@ -20,28 +21,34 @@ func _physics_process(delta):
 	#for if there is no movemetn!!!
 	move_dir = Vector2.ZERO
 	
-	#gets the direction that the character will be moving soon but in a coordinate sorta way
-	var direction = Vector2( Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down") )
-	
-	if enrage_level < 1:
-		pass
-	elif enrage_level < 2:
-		pass
-	else:
-		pass
+	#deals with the vectors of facing at the nearest civilian
 	var nearest_civilian = civilians[0]
 	for civilian in civilians:
 		#finds which civilian is closest to the player
 		if civilian.position.distance_to(position) < nearest_civilian.position.distance_to(position):
 			nearest_civilian = civilian
-	print(nearest_civilian)
+	rage_dir = nearest_civilian.position - position
+	rage_dir = rage_dir.normalized()
+	
+	#gets the direction that the character will be moving soon but in a coordinate sorta way
+	var direction = Vector2( Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down") )
+	
+	if enrage_level < 1:
+		rage_priority = 0.0
+		speed = 50.0
+	elif enrage_level < 2:
+		rage_priority = randf_range(0.2, 0.4)
+		speed = 65.0
+	else:
+		rage_priority = 1.0
+		speed = 100.0
 	
 	#i transfer the value of direction to a different variable because it soon gets changed
 	move_dir = direction
 	if move_dir != Vector2.ZERO:
 		move_dir = move_dir.normalized()
 	
-	linear_velocity += move_dir * SPEED * relevantDelta
+	linear_velocity += ( (move_dir * (1 - rage_priority) ) + (rage_dir * rage_priority) ) * speed * relevantDelta
 	
 	#turn towards the direction of movement
 	var angle_diff = 0.0 
